90607b9af1818d04d228721ff78f36adf3152742
Saeed Alalwan <alalwan@gmail.com>
2014-02-21 22:12:48 -0500

    added more functionality to pong

--- introToPython/miniProject4/pongMore.py
+++ introToPython/miniProject4/pongMore.py
@@ -6,13 +6,16 @@ WHITE = (255,255,255)
 START = (250,250)
 SPEED_INTREVAL=-1.1
 FPS = 100
+MAXNUMOFBALL=3
 
 class Circle():
-    def __init__(self,x,y,size):
+    def __init__(self,surf,x,y,size,s):
+        self.control=s
         self.angle = random.randrange(-45,45)
         self.size= size
+        self.Surf=surf
         self.rect = pg.Rect((0,0),self.size)
-        self.radial_speed = -2
+        self.radial_speed = 2
         self.speed = [self.radial_speed*math.cos(math.radians(self.angle)),
                       self.radial_speed*math.sin(math.radians(self.angle))]
         self.move = [START[0],START[1]]
@@ -32,29 +35,45 @@ class Circle():
     def update(self,Surf,pad):
         self.movement()
         self.rect.topright = self.move
-        self.boundaries(Surf.get_rect(),pad)
+        self.boundaries(pad)
         pg.draw.ellipse(Surf,WHITE,self.rect,5)
+    def thisCyle(self):
+        return self
         
     def movement(self):
         self.move[0] -= self.speed[0]
         self.move[1] += self.speed[1]
-        
-    def boundaries(self,bounding_rect,pad):
+ 
+    def addNew(self):
+        self.control.myCirc.append(Circle(self.Surf,START[0],START[1],(10,15),self))
+              
+    def boundaries(self,pad):
         if(pad.top[1]<=self.rect[1] and pad.down[1]>=self.rect[1] and self.rect[0]<=5):
             self.speed[0] *= SPEED_INTREVAL
+            
             
         
         elif((pad.down[1]>=self.rect[1] or pad.top[1]<=self.rect[1]) and self.rect[0]<=5):
             self.restart()
             
-            print "hit"
-        
-        elif not (0<self.rect[0] <= bounding_rect.size[0]):
-            self.speed[0] *= -1
-           
-        elif not (0<self.rect[1] <= bounding_rect.size[1]):
+        elif not (0<self.rect[0] <= WIDTH):
+            self.ranAngle(random.randrange(-45,45))
+        elif not (0<self.rect[1] <= HEIGHT):
             self.speed[1] *= -1
-         
+            
+    def boundariesBoolean(self,pad):
+        if(pad.top[1]<=self.rect[1] and pad.down[1]>=self.rect[1] and self.rect[0]<=5):
+            return 1
+            
+            
+        
+        elif((pad.down[1]>=self.rect[1] or pad.top[1]<=self.rect[1]) and self.rect[0]<=5):
+            return 2
+        
+        elif not (0<self.rect[0] <= WIDTH):
+            self.ranAngle(random.randrange(-45,45))
+        elif not (0<self.rect[1] <= HEIGHT):
+            self.speed[1] *= -1         
        
 
 class Pad():
@@ -72,22 +91,23 @@ class Pad():
         pg.draw.line(Surf,WHITE,self.top,self.down,self.w)
         
     def moveUP(self):  
-        if not (self.top[1]==0):
-            self.top[1]-=2
-            self.down[1]-=2
+        if not (self.top[1]<=0):
+            self.top[1]-=self.speed
+            self.down[1]-=self.speed
     
         
     def moveDOWN(self):
-        if not (self.down[1]==HEIGHT):
-            self.top[1]+=2
-            self.down[1]+=2
+        if not (self.down[1]>=HEIGHT):
+            self.top[1]+=self.speed
+            self.down[1]+=self.speed
 
 class Control:
     def __init__(self):
         os.environ["SDL_VIDEO_CENTERED"] = '1'
         pg.init()
         self.screen = pg.display.set_mode((WIDTH,HEIGHT))
-        self.myCirc = Circle(START[0],START[1],(10,15))
+        self.myCirc = [Circle(self.screen,START[0],START[1],(10,15),self)]
+        
         self.myPad= Pad(self.screen,[0,250],[0,350],15)
         self.Clock = pg.time.Clock()
         self.fps = FPS
@@ -101,15 +121,33 @@ class Control:
             self.myPad.moveUP()
         elif keys[pg.K_DOWN]:
             self.myPad.moveDOWN()
-        
-                
+    
+    
     def main(self):
         while (True):
             keys = pg.key.get_pressed()
             self.event_loop(keys)
             self.screen.fill(0)
             self.myPad.update(self.screen)
-            self.myCirc.update(self.screen,self.myPad)
+            
+            
+            if(len(self.myCirc)==0):
+                        self.myCirc = [Circle(self.screen,START[0],START[1],(10,15),self)]
+            for i in self.myCirc[:]:
+                    i.update(self.screen,self.myPad)
+                    if (i.boundariesBoolean(self.myPad)==1):
+                        if(len(self.myCirc[:])<MAXNUMOFBALL): self.myCirc.append(Circle(self.screen,START[0],START[1],(10,15),self))
+                        i.update(self.screen,self.myPad)
+                    if (i.boundariesBoolean(self.myPad)==2):
+                        self.myCirc.remove(i)
+
+                        
+                       
+                            
+                   
+                        
+                
+                    
             pg.display.update()
             self.Clock.tick(self.fps)
 
