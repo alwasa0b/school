7a0ef44ec5c01bcda022993f31f10e3241e3a918
Saeed Alalwan <alalwan@gmail.com>
2014-02-28 03:43:41 -0500

    skeleton

--- introToPython/miniProject7/spaceship.py
+++ introToPython/miniProject7/spaceship.py
@@ -1,15 +1,22 @@
-# program template for Spaceship
-import pygame
-import math
-import random
+# implementation of card game - space ship game
+#===============================================================================
+# Author: Saeed Alalwan
+# Version: Beta 1
+#===============================================================================
 
-# globals for user interface
+import urllib2, cStringIO
+from PIL import Image
+import pygame as pg
+import random as rnd
+import sys,os,time,math
+
+# # globals for user interface
 WIDTH = 800
 HEIGHT = 600
 score = 0
 lives = 3
 time = 0.5
-
+# 
 class ImageInfo:
     def __init__(self, center, size, radius = 0, lifespan = None, animated = False):
         self.center = center
@@ -20,69 +27,80 @@ class ImageInfo:
         else:
             self.lifespan = float('inf')
         self.animated = animated
-
+ 
     def get_center(self):
         return self.center
-
+ 
     def get_size(self):
         return self.size
-
+ 
     def get_radius(self):
         return self.radius
-
+ 
     def get_lifespan(self):
         return self.lifespan
-
+ 
     def get_animated(self):
         return self.animated
-
-    
-# art assets created by Kim Lathrop, may be freely re-used in non-commercial projects, please credit Kim
-    
-# debris images - debris1_brown.png, debris2_brown.png, debris3_brown.png, debris4_brown.png
-#                 debris1_blue.png, debris2_blue.png, debris3_blue.png, debris4_blue.png, debris_blend.png
+# 
+#     
+# # art assets created by Kim Lathrop, may be freely re-used in non-commercial projects, please credit Kim
+#     
+# # debris images - debris1_brown.png, debris2_brown.png, debris3_brown.png, debris4_brown.png
+# #                 debris1_blue.png, debris2_blue.png, debris3_blue.png, debris4_blue.png, debris_blend.png
 debris_info = ImageInfo([320, 240], [640, 480])
-debris_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/debris2_blue.png")
-
-# nebula images - nebula_brown.png, nebula_blue.png
+debris_image =pg.image.load(os.path.join('/home/missoula/debris2_blue.png'))
+# 
+# # nebula images - nebula_brown.png, nebula_blue.png
 nebula_info = ImageInfo([400, 300], [800, 600])
-nebula_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_blue.f2013.png")
-
-# splash image
+nebula_image =pg.image.load(os.path.join('/home/missoula/nebula_blue.f2013.png'))
+# nebula_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_blue.f2013.png")
+# 
+# # splash image
 splash_info = ImageInfo([200, 150], [400, 300])
-splash_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/splash.png")
-
-# ship image
+splash_image =pg.image.load(os.path.join('/home/missoula/splash.png'))
+# splash_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/splash.png")
+# 
+# # ship image
 ship_info = ImageInfo([45, 45], [90, 90], 35)
-ship_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/double_ship.png")
-
-# missile image - shot1.png, shot2.png, shot3.png
+ship_image =pg.image.load(os.path.join('/home/missoula/double_ship.png'))
+# ship_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/double_ship.png")
+# 
+# # missile image - shot1.png, shot2.png, shot3.png
 missile_info = ImageInfo([5,5], [10, 10], 3, 50)
-missile_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/shot2.png")
-
-# asteroid images - asteroid_blue.png, asteroid_brown.png, asteroid_blend.png
+missile_image=pg.image.load(os.path.join('/home/missoula/shot2.png'))
+# missile_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/shot2.png")
+# 
+# # asteroid images - asteroid_blue.png, asteroid_brown.png, asteroid_blend.png
 asteroid_info = ImageInfo([45, 45], [90, 90], 40)
-asteroid_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/asteroid_blue.png")
-
-# animated explosion - explosion_orange.png, explosion_blue.png, explosion_blue2.png, explosion_alpha.png
+asteroid_image =pg.image.load(os.path.join('/home/missoula/asteroid_blue.png'))
+# asteroid_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/asteroid_blue.png")
+# 
+# # animated explosion - explosion_orange.png, explosion_blue.png, explosion_blue2.png, explosion_alpha.png
 explosion_info = ImageInfo([64, 64], [128, 128], 17, 24, True)
-explosion_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/explosion_alpha.png")
-
-# sound assets purchased from sounddogs.com, please do not redistribute
-soundtrack = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/soundtrack.mp3")
-missile_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/missile.mp3")
+explosion_image =pg.image.load(os.path.join('/home/missoula/explosion_alpha.png'))
+# explosion_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/explosion_alpha.png")
+# 
+# # sound assets purchased from sounddogs.com, please do not redistribute
+pg.mixer.init()
+soundtrack = pg.mixer.Sound(os.path.join('/home/missoula/soundtrack.mp3'))
+# soundtrack = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/soundtrack.mp3")
+missile_sound = pg.mixer.Sound(os.path.join('/home/missoula/missile.mp3'))
+# missile_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/missile.mp3")
 missile_sound.set_volume(.5)
-ship_thrust_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/thrust.mp3")
-explosion_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/explosion.mp3")
-
+ship_thrust_sound = pg.mixer.Sound(os.path.join('/home/missoula/thrust.mp3'))
+# ship_thrust_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/thrust.mp3")
+explosion_sound = pg.mixer.Sound(os.path.join('/home/missoula/explosion.mp3'))
+# explosion_sound = simplegui.load_sound("http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/explosion.mp3")
+# 
 # helper functions to handle transformations
 def angle_to_vector(ang):
     return [math.cos(ang), math.sin(ang)]
-
+# 
 def dist(p,q):
     return math.sqrt((p[0] - q[0]) ** 2+(p[1] - q[1]) ** 2)
-
-
+# 
+# 
 # Ship class
 class Ship:
     def __init__(self, pos, vel, angle, image, info):
@@ -95,14 +113,14 @@ class Ship:
         self.image_center = info.get_center()
         self.image_size = info.get_size()
         self.radius = info.get_radius()
-        
+#         
     def draw(self,canvas):
         canvas.draw_circle(self.pos, self.radius, 1, "White", "White")
-
+ 
     def update(self):
         pass
-    
-    
+     
+     
 # Sprite class
 class Sprite:
     def __init__(self, pos, vel, ang, ang_vel, image, info, sound = None):
@@ -118,19 +136,20 @@ class Sprite:
         self.animated = info.get_animated()
         self.age = 0
         if sound:
-            sound.rewind()
-            sound.play()
-   
+            pass
+#             sound.rewind()
+#             sound.play()
+    
     def draw(self, canvas):
         canvas.draw_circle(self.pos, self.radius, 1, "Red", "Red")
-    
+     
     def update(self):
         pass        
-
-           
+ 
+            
 def draw(canvas):
     global time
-    
+     
     # animiate background
     time += 1
     wtime = (time / 4) % WIDTH
@@ -139,34 +158,34 @@ def draw(canvas):
     canvas.draw_image(nebula_image, nebula_info.get_center(), nebula_info.get_size(), [WIDTH / 2, HEIGHT / 2], [WIDTH, HEIGHT])
     canvas.draw_image(debris_image, center, size, (wtime - WIDTH / 2, HEIGHT / 2), (WIDTH, HEIGHT))
     canvas.draw_image(debris_image, center, size, (wtime + WIDTH / 2, HEIGHT / 2), (WIDTH, HEIGHT))
-
+ 
     # draw ship and sprites
     my_ship.draw(canvas)
     a_rock.draw(canvas)
     a_missile.draw(canvas)
-    
+     
     # update ship and sprites
     my_ship.update()
     a_rock.update()
     a_missile.update()
-            
-# timer handler that spawns a rock    
+#             
+# # timer handler that spawns a rock    
 def rock_spawner():
     pass
-    
-# initialize frame
-frame = simplegui.create_frame("Asteroids", WIDTH, HEIGHT)
-
+#     
+# # initialize frame
+# frame = simplegui.create_frame("Asteroids", WIDTH, HEIGHT)
+# 
 # initialize ship and two sprites
 my_ship = Ship([WIDTH / 2, HEIGHT / 2], [0, 0], 0, ship_image, ship_info)
 a_rock = Sprite([WIDTH / 3, HEIGHT / 3], [1, 1], 0, 0, asteroid_image, asteroid_info)
 a_missile = Sprite([2 * WIDTH / 3, 2 * HEIGHT / 3], [-1,1], 0, 0, missile_image, missile_info, missile_sound)
-
-# register handlers
-frame.set_draw_handler(draw)
-
-timer = simplegui.create_timer(1000.0, rock_spawner)
-
-# get things rolling
-timer.start()
-frame.start()
+# 
+# # register handlers
+# frame.set_draw_handler(draw)
+# 
+# timer = simplegui.create_timer(1000.0, rock_spawner)
+# 
+# # get things rolling
+# timer.start()
+# frame.start()
